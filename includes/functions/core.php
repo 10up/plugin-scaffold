2f89da43012daad4e78f3d58b10fc3800bec91a6
Dave Ross <dave.ross@10up.com>
2018-07-27 13:57:57 -0400

    style_url needs to return WP_Error on bad context as well

--- includes/functions/core.php
+++ includes/functions/core.php
@@ -113,8 +113,7 @@ function script_url( $script, $context ) {
 function style_url( $stylesheet, $context ) {
 
 	if ( ! in_array( $context, get_enqueue_contexts(), true ) ) {
-		error_log( 'Invalid $context specified in TenUpScaffold stylesheet loader.' );
-		return '';
+		return new WP_Error( 'invalid_enqueue_context', 'Invalid $context specified in TenUpScaffold stylesheet loader.' );
 	}
 
 	return ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ?
