276bdcc1334967974fbd6e4ede2b0e76502a34fd
Jared Rethman <jaredrethman@gmail.com>
2019-10-27 14:55:29 +0200

    Re adding global variable declaration to webpack.common, sans process and module

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,3 +1,5 @@
+/* global require */
+
 const path = require( 'path' );
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
