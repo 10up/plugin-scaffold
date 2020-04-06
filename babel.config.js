cf4019a2f39e47830f5407b341138bc98d5e7ee3
Nícholas Oliveira <nicholas.andre@10up.com>
2020-04-06 14:35:44 -0300

    Updating eslint-config to the latest version

--- babel.config.js
+++ babel.config.js
@@ -1,48 +1,47 @@
 /**
  * Babel Config.
  *
- * @param {Object} api
- * @returns {{presets: {Object}}}
+ * @param {Object} api The bable API
+ * @return {{presets: {Object}}} The babel configuration.
  */
-module.exports = api => {
-
+module.exports = (api) => {
 	/**
-	 * @link https://babeljs.io/docs/en/config-files#apicache
+	 * @see https://babeljs.io/docs/en/config-files#apicache
 	 */
-	api.cache.using( () => 'development' === process.env.NODE_ENV );
+	api.cache.using(() => process.env.NODE_ENV === 'development');
 
 	/**
 	 * Presets
 	 *
-	 * @link https://babeljs.io/docs/en/presets
+	 * @see https://babeljs.io/docs/en/presets
 	 * @type {Array}
 	 */
 	const presets = [
 		[
 			/**
-			 * @link https://babeljs.io/docs/en/babel-preset-env#corejs
+			 * @see https://babeljs.io/docs/en/babel-preset-env#corejs
 			 */
 			'@babel/preset-env',
 			{
 				useBuiltIns: 'usage',
 				corejs: {
 					version: 3,
-					proposals: true
+					proposals: true,
 				},
-			}
+			},
 		],
 	];
 
 	/**
 	 * Plugins
 	 *
-	 * @link https://babeljs.io/docs/en/plugins
+	 * @see https://babeljs.io/docs/en/plugins
 	 * @type {Array}
 	 */
 	const plugins = [];
 
 	return {
 		presets,
-		plugins
+		plugins,
 	};
 };
