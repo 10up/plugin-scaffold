d52ec695d31e73cd4e0f7d9740daa1ca9de9111b
jeffpaul <jeffpaul@hotmail.com>
2020-10-26 09:46:44 -0500

    add dotorg standards to plugin.php

--- plugin.php
+++ plugin.php
@@ -1,9 +1,11 @@
 <?php
 /**
  * Plugin Name:       10up Plugin Scaffold
- * Plugin URI:
+ * Plugin URI:        https://github.com/10up/plugin-scaffold
  * Description:       A brief description of the plugin.
  * Version:           0.1.0
+ * Requires at least: 4.9
+ * Requires PHP:      7.2
  * Author:            10up
  * Author URI:        https://10up.com
  * License:           GPL v2 or later
